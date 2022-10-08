592991afb6598632b4c10d4c6037f84bd219ce6d
IgorekLoschinin <igor.loschinin2014@yandex.ru>
2022-10-08 14:18:17 +0300

    added solve tasks 1 and 2

--- PythonBase/homework5/homework5.py
+++ PythonBase/homework5/homework5.py
@@ -1,42 +1,27 @@
 #!/usr/bin/env python
 # coding: utf-8
-import itertools
-from random import randint
-from decimal import Decimal
-from collections import Counter
+
+from pathlib import Path
+from moduls import rle
+from moduls.utils import read_to_file, write_to_file
 
 
 def main():
 	# """ task1 """
-	# number = float(input("Enter a real number: "))
-	# acc = float(input("Enter the required accuracy '0.0001': "))
-	# print(task1(number, acc))
+	# number = int(input("Number of words: "))
+	# print(task1(number, word_str="авб абв бав абв вба бав вба абв абв абв"))
 
 	# """ task2 """
-	# number = float(input("Enter a real number: "))
-	# acc = float(input("Enter the required accuracy '0.0001': "))
-	# print(task1(number, acc))
+	# file_encode = Path("./text_words.txt")
+	# file_out = Path("./text_code_words.txt")
+	# task2(file_encode, file_out, mode="encode")
 
-	# """ task3 """
-	# number = float(input("Enter a real number: "))
-	# acc = float(input("Enter the required accuracy '0.0001': "))
-	# print(task1(number, acc))
-
-	# """ task4 """
-	# number = float(input("Enter a real number: "))
-	# acc = float(input("Enter the required accuracy '0.0001': "))
-	# print(task1(number, acc))
-
-	# """ task5 """
-	# number = float(input("Enter a real number: "))
-	# acc = float(input("Enter the required accuracy '0.0001': "))
-	# print(task1(number, acc))
-	
-	pass
+	print("Uncomment Tasks!")
 
 
-def task1() -> None:
-	""" Напишите программу, удаляющую из текста все слова, содержащие "абв". В тексте используется разделитель пробел.
+def task1(num_word: int, word_str: str = None, opt: int = 1) -> str | None:
+	""" Напишите программу, удаляющую из текста все слова, содержащие "абв".
+	В тексте используется разделитель пробел.
 
 	in
 	Number of words: 10
@@ -59,11 +44,42 @@ def task1() -> None:
 	The data is incorrect
 
 	"""
-	pass
+
+	pattern = "абв"
+
+	if num_word < 0:
+		print("The data is incorrect!")
+		return None
+
+	if word_str is None:
+		str_words = " ".join([
+			input("Enter word: ").strip() for _ in range(num_word)
+		])
+
+	else:
+		str_words = word_str
+
+	print(str_words)
+
+	match opt:
+		case 1:
+			return " ".join([
+				item.strip()
+				for item in str_words.replace(pattern, "").split(" ")
+				if len(item.strip()) != 0
+			]).strip()
+
+		case 2:
+			return " ".join([
+				item
+				for item in str_words.split(" ")
+				if item != pattern
+			])
 
 
-def task2() -> None:
-	""" Реализуйте RLE алгоритм: реализуйте модуль сжатия и восстановления данных.Входные и выходные данные хранятся в отдельных текстовых файлах.
+def task2(file_in: Path, file_out: Path, mode: str = None) -> None:
+	""" Реализуйте RLE алгоритм: реализуйте модуль сжатия и восстановления
+	данных.Входные и выходные данные хранятся в отдельных текстовых файлах.
 
 	Алгоритм RLE
 
@@ -85,11 +101,23 @@ def task2() -> None:
 	vbbwwPPuuuTTYyWWQQ
 
 	'text_code_words.txt'
-	5a29v4s3D3d2F4g2O3i2a1
+	5a29v4s3D3d2F4g2O3i2a
 	1v2b2w2P3u2T1Y1y2W2Q
 
 	"""
-	pass
+
+	if not (file_in.is_file() and file_in.exists()):
+		print("Input file not exists!")
+		return None
+
+	data = list(map(lambda x: x.strip(), read_to_file(file_in)))
+
+	match mode:
+		case "encode":
+			write_to_file(file_out, [rle.encode(d) + "\n" for d in data])
+
+		case "decode":
+			data_decoding = rle.decode()
 
 
 def task3() -> None:
--- PythonBase/tests/tests_hw5.py
+++ PythonBase/tests/tests_hw5.py
@@ -1,23 +1,60 @@
 import unittest
-import homework5 as hw5
+from homework5 import homework5 as hw5
+from moduls.rle import encode, decode
 
 
-class HomeWork3(unittest.TestCase):
+class HomeWork5(unittest.TestCase):
 
 	def test_task1(self):
-		self.assertEqual(hw5.task1([]), 0)
+		self.assertEqual(
+			hw5.task1(10, word_str="авб абв бав абв вба бав вба абв абв абв"),
+			"авб бав вба бав вба"
+		)
+		self.assertEqual(
+			hw5.task1(
+				10,
+				word_str="авб абв бав абв вба бав вба абв абв абв",
+				opt=2
+			),
+			"авб бав вба бав вба"
+		)
 
-	def test_task2(self):
-		self.assertEqual(hw5.task2([]), 0)
+		self.assertEqual(
+			hw5.task1(6, word_str="ваб вба абв ваб бва абв"),
+			"ваб вба ваб бва"
+		)
+		self.assertEqual(
+			hw5.task1(6, word_str="ваб вба абв ваб бва абв", opt=2),
+			"ваб вба ваб бва"
+		)
 
-	def test_task3(self):
-		self.assertEqual(hw5.task3([]), 0)
+		self.assertIsNone(
+			hw5.task1(-1, word_str="авб абв бав абв вба бав вба абв абв абв")
+		)
 
-	def test_task4(self):
-		self.assertEqual(hw5.task4([]), 0)
+	def test_rle_encode(self):
+		self.assertIsNone(encode(""))
+		self.assertEqual(encode("a"), "1a")
+		self.assertEqual(encode("aF"), "1a1F")
+		self.assertEqual(encode("ABCD"), "1A1B1C1D")
+		self.assertEqual(encode(
+			"aaaaavvvvvvvvvvvvvvvvvvvvvvvvvvvvvssssDDDdddFFggggOOiiiaa"
+		), "5a29v4s3D3d2F4g2O3i2a")
+		self.assertEqual(encode(
+			"vbbwwPPuuuTTYyWWQQ"
+		), "1v2b2w2P3u2T1Y1y2W2Q")
 
-	def test_task5(self):
-		self.assertEqual(hw5.task5([]), 0)
+	def test_rle_decode(self):
+		self.assertIsNone(decode(""))
+		self.assertEqual(decode("1a"), "a")
+		self.assertEqual(decode("1a1F"), "aF")
+		self.assertEqual(decode("1A1B1C1D"), "ABCD")
+		self.assertEqual(decode(
+			"5a29v4s3D3d2F4g2O3i2a"
+		), "aaaaavvvvvvvvvvvvvvvvvvvvvvvvvvvvvssssDDDdddFFggggOOiiiaa")
+		self.assertEqual(decode(
+			"1v2b2w2P3u2T1Y1y2W2Q"
+		), "vbbwwPPuuuTTYyWWQQ")
 
 
 if __name__ == '__main__':
