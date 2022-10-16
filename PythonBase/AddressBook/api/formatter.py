9e2309aa8a12db652ae49979e4047b0563b0e253
IgorekLoschinin <igor.loschinin2014@yandex.ru>
2022-10-17 00:53:23 +0300

    Fixing and refactoring 1

--- PythonBase/AddressBook/api/formatter.py
+++ PythonBase/AddressBook/api/formatter.py
@@ -2,9 +2,14 @@
 # coding: utf-8
 
 import csv
+
+from pathlib import Path
 from lxml import etree
 from json import dumps, loads
 from xml.etree import ElementTree as ET
+from AddressBook.moduls.logger import logger
+
+log = logger(__name__, "formatter.log")
 
 
 class Formatter(object):
@@ -12,7 +17,7 @@ class Formatter(object):
 	def __init__(self) -> None:
 		self._obj = None
 
-	def handle(self, format_d: str = None):
+	def handle(self, format_d: str = None) -> None:
 		"""  """
 		if format_d is None:
 			format_d = ""
@@ -29,6 +34,17 @@ class Formatter(object):
 
 			case _:
 				print("Не верный формат!")
+				log.warning("Не верный формат!")
+
+	@staticmethod
+	def _check_file(file: str | Path) -> bool:
+		"""  """
+		if not (Path(file).is_file() and Path(file).exists()):
+			print("File is not exists!")
+			log.error("File is not exists!")
+			return False
+
+		return True
 
 	def write(self, data: dict, file: str) -> None:
 		"""  """
@@ -36,7 +52,7 @@ class Formatter(object):
 		if self._obj is not None:
 			self._obj.to_file(data, file)
 
-	def read(self, file: str) -> dict:
+	def read(self, file: str) -> dict | None:
 		"""  """
 
 		if self._obj is not None:
@@ -51,7 +67,14 @@ class XML(object):
 	@staticmethod
 	def from_file(file: str) -> dict:
 		"""  """
-		tree = etree.parse(f'{file}.xml')
+
+		file_name = f'{file}.xml'
+
+		if not (Path(file_name).is_file() and Path(file_name).exists()):
+			print("File is not exists!")
+			log.error("File is not exists!")
+
+		tree = etree.parse(file_name)
 		root = tree.getroot()
 
 		return {
@@ -73,7 +96,9 @@ class XML(object):
 
 		tree = ET.ElementTree(root)
 		ET.indent(tree, space="\t", level=0)
-		tree.write(f'{file}.xml', xml_declaration=True, encoding="utf-8")
+
+		with open(f'{file}.xml', 'wb') as descript_file:
+			tree.write(descript_file, xml_declaration=True, encoding="utf-8")
 
 
 class CSV(object):
@@ -95,11 +120,16 @@ class CSV(object):
 	def to_file(data: dict, file: str) -> None:
 		"""  """
 
-		header = ['lastName', 'name', 'patronymic', 'phoneNumber']
+		header = ['id', 'lastName', 'name', 'patronymic', 'phoneNumber']
 		with open(f'{file}.csv', 'w') as file:
 			writer = csv.DictWriter(file, fieldnames=header)
 			writer.writeheader()
-			writer.writerows(data.values())
+
+			data_write = [
+				{"id": id_, **info_d} for id_, info_d in data.items()
+			]
+
+			writer.writerows(data_write)
 
 
 class Json(object):
--- PythonBase/AddressBook/moduls/logger.py
+++ PythonBase/AddressBook/moduls/logger.py
@@ -1,15 +1,36 @@
 #!/usr/bin/env python
 # coding: utf-8
+import logging
 
-from AddressBook.moduls.utils import write_to_file
+# from AddressBook.moduls.utils import write_to_file
+
+_log_format = \
+    f"%(asctime)s - [%(levelname)s] - %(name)s - " \
+    f"(%(filename)s).%(funcName)s(%(lineno)d) - %(message)s"
 
 
-def logger(func):
-	def wrapper(*args, **kwargs):
-		try:
-			func(*args, **kwargs)
+# def loggerV1(func):
+# 	def wrapper(*args, **kwargs):
+# 		try:
+# 			func(*args, **kwargs)
+#
+# 		except Exception as e:
+# 			write_to_file("log.txt", f"{e}\n", mode="a")
+#
+# 	return wrapper
 
-		except Exception as e:
-			write_to_file("log.txt", f"{e}\n", mode="a")
 
-	return wrapper
+def logger(name: str, file: str):
+    """ Логер """
+
+    log = logging.getLogger(name)
+    log.setLevel(logging.INFO)
+
+    file_handler = logging.FileHandler(file, mode="w")
+    file_handler.setLevel(logging.INFO)
+    file_handler.setFormatter(logging.Formatter(_log_format))
+
+    log.addHandler(file_handler)
+
+    return log
+
